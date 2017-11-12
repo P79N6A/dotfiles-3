b3778595c178d1d7dc4aac7252252dd0b5f2de2f
Rick Yu <cosmtrek@gmail.com>
2017-11-12 12:51:33 +0800

    Update...

--- hack/setup.sh
+++ hack/setup.sh
@@ -5,10 +5,12 @@ brew=/usr/local/bin/brew
 # Homebrew
 `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
 
-`${brew} install ack ssh-copy-id openssl git cmake git-flow cowsay tmux ctags tree wget fortune thefuck luajit python3`
+`${brew} install ack ssh-copy-id openssl git cmake git-flow cowsay tmux ctags tree wget fortune thefuck tldr luajit python3 mas glide`
 
 `${brew} install vim --with-luajit --with-python3`
 `${brew} install macvim --with-luajit --with-python3`
+
+`${brew} cask install keepingyouawake`
 
 # Zsh plugin manager(https://github.com/zplug/zplug)
 `curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh`
