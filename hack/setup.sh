c97aef908e960182f85b96f370a9397f75f508d0
Rick Yu <cosmtrek@gmail.com>
2018-01-05 13:40:16 +0800

    Install GNU tools

--- hack/setup.sh
+++ hack/setup.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-
-brew=/usr/local/bin/brew
+#!/usr/bin/env bash
 
 # Homebrew
 `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
 
-`${brew} install ack ssh-copy-id openssl git cmake git-flow cowsay tmux ctags tree wget fortune thefuck tldr luajit pyenv python3 mas`
+`brew install ssh-copy-id openssl ack cmake git git-flow cowsay tmux ctags tree wget fortune thefuck tldr luajit pyenv python3 mas`
 
-`${brew} cask install keepingyouawake`
+`brew cask install keepingyouawake`
+
+# install gnu tools
+`/bin/bash ./hack/install-gnu.sh`
 
 # Zsh plugin manager(https://github.com/zplug/zplug)
 `curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh`
